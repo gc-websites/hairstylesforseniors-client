3fdf45b46e3c0392d4bc72d8404e5db4d7ab2d87
Назар Івченко <nazary9911@gmail.com>
2025-10-22 12:25:49 +0300

    feature/lead

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 
 const Product = () => {
+  const params = new URLSearchParams(window.location.search);
+  const fbclid = params.get('fbclid');
   const url = window.location.href;
   const id = url.split('/').pop();
   const [productData, setProductData] = useState({});
+  const [clickId, setClickId] = useState('');
+  const [urlWithSubtag, setUrlWithSubtag] = useState('');
 
   useEffect(() => {
     try {
@@ -25,12 +29,42 @@ const Product = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if (fbclid) {
+      try {
+        fetch(`http://localhost:4000/get-product/ads/${id}`, {
+          headers: { 'Content-Type': 'application/json' },
+          method: 'POST',
+          body: JSON.stringify({ fbclid: fbclid }),
+        })
+          .then(res => {
+            if (!res.ok) {
+              throw new Error('Failed to fetch products');
+            }
+            return res.json();
+          })
+          .then(data => {
+            setClickId(data.data);
+          });
+      } catch (err) {
+        console.error('❌ Error:', err);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (productData && clickId) {
+      setUrlWithSubtag(productData?.link + `&subtag08-20=${clickId}`);
+      console.log(urlWithSubtag);
+    }
+  }, [clickId, productData]);
+
   return (
     <div className="flex flex-col justify-center items-center p-5">
       <img
         id="cta-image"
         src={productData?.image?.url}
-        onClick={() => window.open(productData?.link, '_blank')}
+        onClick={() => window.open(urlWithSubtag, '_blank')}
         className="w-[90vw] md:w-[40vw] rounded-xl cursor-pointer transition-transform duration-300 ease-in-out hover:-translate-y-2 hover:shadow-xl"
       />
       <h1 className="text-2xl md:text-3xl text-center font-bold w-[90%]">
@@ -47,7 +81,7 @@ const Product = () => {
         id="cta-btn"
         className="w-[90vw] md:w-[40vw] p-5 m-5 rounded bg-[rgb(3,145,133)] text-2xl font-bold relative bg-[#eaa31e] border border-black rounded-lg py-[15px] px-[20px] mb-[10px] flex justify-center items-center cursor-pointer transition-colors duration-300 font-bold overflow-hidden text-[17px] hover:bg-[#c47f00]
           before:content-[''] before:absolute before:top-[-150%] before:left-[-150%] before:w-full before:h-[50%] before:bg-[rgba(255,255,255,0.3)] before:-rotate-45 before:animate-myshine font-inter"
-        onClick={() => window.open(productData?.link, '_blank')}
+        onClick={() => window.open(urlWithSubtag, '_blank')}
       >
         VIEW ON AMAZON
       </button>
